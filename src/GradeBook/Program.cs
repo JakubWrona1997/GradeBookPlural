cfd35cb2214bd88390fb19b250326c601ec131c0
DESKTOP-BK2QFK7\Jakub <jakub.wrona97@gmail.com>
2021-11-01 01:29:06 +0100

    First Commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Jakob");
+            book.AddGrade(56.1);
+            book.AddGrade(31.5);
+            
+            var stats = book.GetStatistics();
+            
+            Console.WriteLine($"Lowest grade is {stats.Low}, Highest is {stats.High},Average:{stats.Average}");     
+            // foreach (var item in grades)
+            // {
+            //     Console.WriteLine(item);
+            // }
+
+            // if(args.Length > 0)
+            // {
+            // Console.WriteLine($"Hello {args[0]}");  
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Hello!");
+            // }
+           
+        }
+    }
+}
