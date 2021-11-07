fa740fe16d93c6248870643944f86e18c5182f7c
DESKTOP-BK2QFK7\Jakub <jakub.wrona97@gmail.com>
2021-11-07 15:48:42 +0100

    Delegate test

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -4,9 +4,29 @@ using Xunit;
 
 namespace GradeBook.Tests
 {
+
+    public delegate string WriteLogDelegate(string logMessage);
+
+    
     public class TypeTests
     {           
-            
+        [Fact]
+
+        public void WriteLogDelegateCanPointToMethod()
+        {
+            WriteLogDelegate log;
+            log = ReturnMessage;
+            var result = log("Hello");            
+
+            Assert.Equal("Hello", result);
+        }
+
+        private string ReturnMessage(string message)
+        {
+            return message;
+        }  
+
+
         [Fact]
 
         public void ValueTest()
